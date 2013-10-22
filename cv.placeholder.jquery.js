413aa4988bdb95e8904e46746a7232ce0a37988f
Joe Pour <joe.pour@gmail.com>
2013-10-22 18:01:39 +1100

    fixed indentation

--- cv.placeholder.jquery.js
+++ cv.placeholder.jquery.js
@@ -9,14 +9,14 @@
 
 		//
 		// Options
-        options = (options || {});
+		options = (options || {});
 		self.options = $.extend({}, self.defaults, options);
 
-        //
-        // Assign placeholder mask url to a sane default
-        self.options.maskImageUrl = (options.maskImageUrl)
-            ? options.maskImageUrl
-            : (options.placeholderImageUrl || self.defaults.placeholderImageUrl);
+		//
+		// Assign placeholder mask url to a sane default
+		self.options.maskImageUrl = (options.maskImageUrl)
+			? options.maskImageUrl
+			: (options.placeholderImageUrl || self.defaults.placeholderImageUrl);
 
 		// Constructor
 		self.init.apply(self, arguments);
@@ -25,110 +25,105 @@
 	Placeholder.prototype = {
 
 		defaults: {
-            showMask:            false,
-            maskImageUrl:        'http://thumbs.clickview.com.au/placeholder',
-            placeholderImageUrl: 'http://thumbs.clickview.com.au/placeholder'
-        },
+			showMask:            false,
+			maskImageUrl:        'http://thumbs.clickview.com.au/placeholder',
+			placeholderImageUrl: 'http://thumbs.clickview.com.au/placeholder'
+		},
 
-        style: function(){
-            this.$el.css({
+		style: function(){
+			this.$el.css({
 				'width':            this.$el.width(),
 				'height':           this.$el.height(),
-                'display':          'block',
-                'background-image': 'url(' + this.options.placeholderImageUrl + ')'
+				'display':          'block',
+				'background-image': 'url(' + this.options.placeholderImageUrl + ')'
 			});
 		},
 
-        onError: function(){
-            this.$el.on('error', this, function(e){
-                e.data.$el.attr('src', e.data.options.placeholderImageUrl);
+		onError: function(){
+			this.$el.on('error', this, function(e){
+				e.data.$el.attr('src', e.data.options.placeholderImageUrl);
 
-                if(e.data.options.showMask){
-                    e.data.$el.next('.cv-placeholder-mask').remove();
-                }
-            });
-        },
+				if(e.data.options.showMask){
+					e.data.$el.next('.cv-placeholder-mask').remove();
+				}
+			});
+		},
 
-        onSuccess: function(){
-            this.$el.on('load', this, function(e){
-                if(e.data.options.showMask){
-                    e.data.$el.next('.cv-placeholder-mask').remove();
-                }
-            });
-        },
+		onSuccess: function(){
+			this.$el.on('load', this, function(e){
+				if(e.data.options.showMask){
+					e.data.$el.next('.cv-placeholder-mask').remove();
+				}
+			});
+		},
 
 		init: function(){
 
-            //
-            // Register event handlers
-            this.onError();
-            this.onSuccess();
+			//
+			// Register event handlers
+			this.onError();
+			this.onSuccess();
 
-            //
-            // Apply placeholder styles
-            this.style();
+			//
+			// Apply placeholder styles
+			this.style();
 
-            //
-            // Add placeholder mask if enabled
-            if(this.options.showMask){
-                var mask = new PlaceholderMask(this.$el, this.options)
-            }
+			//
+			// Add placeholder mask if enabled
+			if(this.options.showMask){
+				var mask = new PlaceholderMask(this.$el, this.options)
+			}
 		}
 	};
 
-    var PlaceholderMask = function(element, options){
-        var self = (this instanceof PlaceholderMask) ? this : Object.create(PlaceholderMask.prototype);
+	var PlaceholderMask = function(element, options){
+		var self = (this instanceof PlaceholderMask) ? this : Object.create(PlaceholderMask.prototype);
 
-        self.$el = $(element);
+		self.$el = $(element);
 
-        self.options = $.extend({}, self.defaults, options);
+		self.options = $.extend({}, self.defaults, options);
 
-        self.init.apply(self, arguments);
-    };
+		self.init.apply(self, arguments);
+	};
 
-    PlaceholderMask.prototype = {
-        defaults: {},
+	PlaceholderMask.prototype = {
+		defaults: {},
 
-        style: function(){
-            this.$el
-                .next('.cv-placeholder-mask')
-                .css({
-                    'top':              0,
-                    'left':             0,
-                    'width':            this.$el.width(),
-                    'height':           this.$el.height(),
-                    'z-index':          9998,
-                    'display':          'block',
-                    'position':         'absolute',
-                    'background-image': 'url(' + this.options.maskImageUrl + ')'
-                 });
-        },
+		style: function(){
+			this.$el
+				.next('.cv-placeholder-mask')
+				.css({
+					'top':              0,
+					'left':             0,
+					'width':            this.$el.width(),
+					'height':           this.$el.height(),
+					'z-index':          9998,
+					'display':          'block',
+					'position':         'absolute',
+					'background-image': 'url(' + this.options.maskImageUrl + ')'
+				});
+		},
 
-        init: function() {
-            //
-            // We need to create a container to hold
-            this.$el.wrap('<div class="cv-placeholder-container"></div>');
-            $('.cv-placeholder-container').css('position', 'relative');
-
-            //
-            // Add mask to DOM
-            this.$el.after(jQuery('<span/>').addClass('cv-placeholder-mask'));
-
-            //
-            // Apply styles to mask
-            this.style();
-        }
-    };
+		init: function() {
+			//
+			// We need to create a container to hold
+			this.$el.wrap('<div class="cv-placeholder-container"></div>');
+			$('.cv-placeholder-container').css('position', 'relative');
+			
+			//
+			// Add mask to DOM
+			this.$el.after(jQuery('<span/>').addClass('cv-placeholder-mask'));
+			
+			//
+			// Apply styles to mask
+			this.style();
+		}
+		};
 
 	$.fn.cvPlaceholder = function( option ){
-        return this.each(function () {
-            var placeholder = new Placeholder(this, option);
-        });
+		return this.each(function () {
+			var placeholder = new Placeholder(this, option);
+		});
 	};
 
 }( jQuery ));
-
-
-$(function(){
-   $('.item img').cvPlaceholder();
-});
